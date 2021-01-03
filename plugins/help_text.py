edc35d73101c65b3b6d9969062c98a4ad3506951
Aswanth VK <55796045+AswanthVK@users.noreply.github.com>
2021-01-03 17:02:09 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -87,7 +87,13 @@ async def rename_cb(bot, update):
     try:
         filename = file.file_name
     except:
-        filename = "Not Available"    
+        filename = "Not Available"  
+    elif (tldextract.extract(m.text)).domain not in youtube_dl_links:
+            file_size = header_info.get("Content-Length") if "Content-Length" in header_info else None
+            if file_size is not None and int(file_size) > 2147483648:
+                await m.reply_text(
+                    f"Well that file is bigger than I can upload to telegram!"
+                )
     
     await bot.send_message(
         chat_id=update.chat.id,
