4de734d2225f3e95d5bec7a11b9dc34823bb6b2f
Aswanth VK <55796045+AswanthVK@users.noreply.github.com>
2021-02-07 09:15:41 +0530

    Update rename_file.py

--- plugins/rename_file.py
+++ plugins/rename_file.py
@@ -67,16 +67,17 @@ async def rename_doc(bot, message):
     except:
         extension = "mkv" 
  
-    def get_size(size):
-    """Get size in readable format"""
+    try:
+        get_size(size):
+        """Get size in readable format"""
 
-    units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
-    size = float(size)
-    i = 0
-    while size >= 1024.0 and i < len(units):
-        i += 1
-        size /= 1024.0
-    return "%.2f %s" % (size, units[i])
+        units = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB"]
+        size = float(size)
+        i = 0
+        while size >= 1024.0 and i < len(units):
+            i += 1
+            size /= 1024.0
+        return "%.2f %s" % (size, units[i])
 
     await bot.delete_messages(
         chat_id=message.chat.id,
