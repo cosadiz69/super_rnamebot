08a42e1957d9dfb2480d75942e05d5c0da7ecd40
Aswanth VK <55796045+AswanthVK@users.noreply.github.com>
2021-04-04 10:27:15 +0530

    Update generate_screen_shot.py

--- plugins/generate_screen_shot.py
+++ plugins/generate_screen_shot.py
@@ -24,7 +24,7 @@ from helper_funcs.help_Nekmo_ffmpeg import generate_screen_shots
 from plugins.helpers import progress_for_pyrogram
 
 
-@pyrogram.Client.on_message(pyrogram.Filters.command(["generatescss"]))
+@Client.on_message(pyrogram.Filters.command(["generatescss"]))
 async def generate_screen_shot(bot, update):
     if update.from_user.id not in Config.AUTH_USERS:
         await bot.delete_messages(
