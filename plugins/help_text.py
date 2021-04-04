02429c9d3be9ff23c7e4ec7a4b60274a740d7864
Aswanth VK <55796045+AswanthVK@users.noreply.github.com>
2021-04-04 16:23:13 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -60,7 +60,7 @@ def upgrade(bot, update):
 
     bot.send_message(
         chat_id=update.chat.id,
-        text=script.UPGRADE_TEXT,
+        text=script.ABOy_TEXT,
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Close", callback_data="cancel_e")]]),
         parse_mode="html",
         reply_to_message_id=update.message_id,
@@ -92,5 +92,5 @@ async def cancel_extract(bot, update):
     
     await bot.send_message(
         chat_id=update.chat.id,
-        text="Process Cancelled 🙃",
+        text=" ",
     )
