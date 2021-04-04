38aacf60d1773fc80b44f380c16e1fe3f74a1151
Aswanth VK <55796045+AswanthVK@users.noreply.github.com>
2021-04-04 10:31:57 +0530

    Update generate_screen_shot.py

--- plugins/generate_screen_shot.py
+++ plugins/generate_screen_shot.py
@@ -20,7 +20,7 @@ from script import script
 import pyrogram
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
-from pyrogram import Client, filters
+from pyrogram import Client, Filters
 from helper_funcs.help_Nekmo_ffmpeg import generate_screen_shots
 from plugins.helpers import progress_for_pyrogram
 
