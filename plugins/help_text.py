4fd2d4a5bc0b78ab5e93f5c0e4b8aa6715d08765
Aswanth VK <55796045+AswanthVK@users.noreply.github.com>
2021-04-04 16:09:45 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -1,1 +1,95 @@
-import logging logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s') logger = logging.getLogger(__name__) logging.getLogger("pyrogram").setLevel(logging.WARNING) import time import os import sqlite3 import asyncio if bool(os.environ.get("WEBHOOK", False)): from sample_config import Config else: from config import Config from script import script import pyrogram from pyrogram import Client, filters from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply, CallbackQuery from pyrogram.errors import UserNotParticipant from plugins.rename_file import rename_doc @Client.on_message(filters.command(["help"])) def help_user(bot, update): bot.send_message( chat_id=update.chat.id, text=script.HELP_USER, reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton("About", callback_data="about_data"), InlineKeyboardButton("Close", callback_data="cancel_e"), ] ] ), reply_to_message_id=message.message_id ) except: pass @Client.on_message(filters.command(["start"])) def send_start(bot, update): # logger.info(update) bot.send_message( chat_id=update.chat.id, text=script.START_TEXT.format(update.from_user.first_name), reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton("Project Channel", url="https://t.me/TheSuperBots"), InlineKeyboardButton("Help", callback_data="help_data"), ], [ InlineKeyboardButton("Creator", url="https://t.me/AswanthVK"), ] ] ), reply_to_message_id=message.message_id ) except: pass @Client.on_message(filters.command(["upgrade"])) def upgrade(bot, update): # logger.info(update) bot.send_message( chat_id=update.chat.id, text=script.UPGRADE_TEXT, parse_mode="html", reply_to_message_id=update.message_id, disable_web_page_preview=True ) @Client.on_message(filters.command(["about"])) def upgrade(bot, update): # logger.info(update) bot.send_message( chat_id=update.chat.id, text=script.ABOUT_TEXT, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Close", callback_data="cancel_e")]]), parse_mode="html", reply_to_message_id=update.message_id, disable_web_page_preview=True ) @Client.on_message(filters.private & (filters.document | filters.video | filters.audio | filters.voice | filters.video_note)) async def rename_cb(bot, update): file = update.document or update.video or update.audio or update.voice or update.video_note try: filename = file.file_name except: filename = "Not Available" await bot.send_message( chat_id=update.chat.id, text="<b>File Name</b> : <code>{}</code> \n\nSelect the desired option below 👇".format(filename), reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Rename", callback_data="rename_button"), InlineKeyboardButton("Cancel", callback_data="cancel_e")], [InlineKeyboardButton("Screenshots", callback_data="screnshots_data")]]), parse_mode="html", reply_to_message_id=update.message_id, disable_web_page_preview=True ) async def cancel_extract(bot, update): await bot.send_message( chat_id=update.chat.id, text="", )
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+import time
+import os
+import sqlite3
+import asyncio
+
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+from script import script
+
+import pyrogram
+
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, ForceReply
+from pyrogram.errors import UserNotParticipant
+
+from plugins.rename_file import rename_doc
+
+
+@Client.on_message(filters.command(["help"]))
+def help_user(bot, update):
+    bot.send_message(
+        chat_id=update.chat.id,
+        text=script.HELP_USER,
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="About", callback_data="help_data"),
+                         [InlineKeyboardButton(text="Close", callback_data="cancel_e")]]),
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
+
+
+@Client.on_message(filters.command(["start"]))
+def send_start(bot, update):
+    # logger.info(update)
+    
+    bot.send_message(
+        chat_id=update.chat.id,
+        text=script.START_TEXT.format(update.from_user.first_name),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Project Channel", url="https://t.me/TheSuperBots")],
+                         InlineKeyboardButton(text="Help", callback_data="help_data"),
+                         InlineKeyboardButton(text="About", callback_data="about_data)]]),
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
+
+
+@Client.on_message(filters.command(["upgrade"]))
+def upgrade(bot, update):
+    # logger.info(update)
+
+    bot.send_message(
+        chat_id=update.chat.id,
+        text=script.UPGRADE_TEXT,
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+        disable_web_page_preview=True
+    )
+
+    
+@Client.on_message(filters.private & (filters.document | filters.video | filters.audio | filters.voice | filters.video_note))
+async def rename_cb(bot, update):
+ 
+    file = update.document or update.video or update.audio or update.voice or update.video_note
+    try:
+        filename = file.file_name
+    except:
+        filename = "Not Available"
+    
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text="<b>File Name</b> : <code>{}</code> \n\nSelect the desired option below 😇".format(filename),
+        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Rename", callback_data="rename_button"),
+                                                InlineKeyboardButton(text="Cancel", callback_data="cancel_e")]]),
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+        disable_web_page_preview=True   
+    )   
+
+
+async def cancel_extract(bot, update):
+    
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text="Process Cancelled 🙃",
+    )
