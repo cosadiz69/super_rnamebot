21ea18c882a8a6165997c37c07a825606041920d
Aswanth VK <55796045+AswanthVK@users.noreply.github.com>
2021-04-04 14:00:55 +0530

    Update script.py

--- script.py
+++ script.py
@@ -24,3 +24,9 @@ Note: You must subscribe My updates Channel for using me.
 1. Send me any Telegram File.
 2. Choose appropriate option."""
 
+    ABOUT_TEXT = """<b>• Name</b> : <code> Rename Bot</code>
+<b>• Creator</b> : <b><a href="https://t.me/AswanthVK">AswanthVK</a></b>
+<b>• Language</b> : <code>Python3</code>
+<b>• Library</b> : <i><a href="https://docs.pyrogram.org">Pyrogram Asyncio 1.13.0 </a></i>
+<b>• Server</b> : <i><a href="https://herokuapp.com/">Heroku </a></i>
+<b>• Source Code</b> : <i><a href="https://github.com/AswanthVK/Super-RenameBot">Click Here</a></i>
