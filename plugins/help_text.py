ca8b37a6f919068fc10c7f80bf994d607aabd4f7
Aswanth VK <55796045+AswanthVK@users.noreply.github.com>
2021-04-04 15:36:25 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -86,7 +86,6 @@ def upgrade(bot, update):
     bot.send_message(
         chat_id=update.chat.id,
         text=script.ABOUT_TEXT,
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Close", callback_data="cancel_e")]]),
         parse_mode="html",
         reply_to_message_id=update.message_id,
         disable_web_page_preview=True
@@ -105,9 +104,6 @@ async def rename_cb(bot, update):
     await bot.send_message(
         chat_id=update.chat.id,
         text="<b>File Name</b> : <code>{}</code>  \n\nSelect the desired option below 👇".format(filename),
-        reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton("Rename", callback_data="rename_button"),
-                                                InlineKeyboardButton("Cancel", callback_data="cancel_e")],
-                                           [InlineKeyboardButton("Screenshots", callback_data="screnshots_data")]]),
         parse_mode="html",
         reply_to_message_id=update.message_id,
         disable_web_page_preview=True   
