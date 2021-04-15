76e19a729f7be3ea23470275d40ec0ca8bfd1426
Aswanth VK <55796045+AswanthVK@users.noreply.github.com>
2021-04-16 03:29:09 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -80,7 +80,7 @@ async def rename_cb(bot, update):
     
     await bot.send_message(
         chat_id=update.chat.id,
-        text="<b>File Name</b> : <code>{}</code>  \n<b>Size</b>: `{humanbytes(size)}` \n\nSelect the desired option below 😇".format(filename),
+        text="<b>File Name</b> : <code>{}</code>  \n<b>Size</b>: {humanbytes(size)} \n\nSelect the desired option below 😇".format(filename),
         reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="Rename", callback_data="rename_button"),
                                                 InlineKeyboardButton(text="Cancel", callback_data="cancel_e")]]),
         parse_mode="html",
